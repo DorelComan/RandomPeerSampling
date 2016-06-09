fb990141836079fad9610630a2fa917d3306d56e
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2016-06-09 12:17:05 +0200

    Setup a server for QUERY REQUESTS and FOR PULL REQUESTS for local view

--- src/main/java/Rps.java
+++ src/main/java/Rps.java
@@ -1,8 +1,3 @@
-import io.netty.buffer.ByteBuf;
-import io.netty.handler.logging.LogLevel;
-import io.reactivex.netty.protocol.tcp.server.TcpServer;
-import rxnetty.ExamplesEnvironment;
-
 /**
  * @author Hannes Dorfmann
  */
@@ -10,31 +5,15 @@ public class Rps {
 
   public static void main(final String[] args) {
 
-    setupQueryReceiver(args);
-  }
+    // TODO read config
 
-  public static void setupQueryReceiver(final String[] args) {
-    ExamplesEnvironment env = ExamplesEnvironment.newEnvironment(Rps.class);
+    QueryServer queryServer = new QueryServer(11001);
+    PullServer pullServer = new PullServer(11002);
 
-    QueryReceiverServer queryReceiverServer = new QueryReceiverServer();
+    queryServer.start();
+    pullServer.start();
 
-        /*Starts a new TCP server on an ephemeral port.*/
-    TcpServer<ByteBuf, ByteBuf> server = TcpServer.newServer(0)
-        .enableWireLogging(LogLevel.DEBUG)
-        .start(
-            connection ->
-                connection.writeBytesAndFlushOnEach(connection.getInput()
-                    .doOnNext(byteBuf -> System.out.println("New incomming message"))
-                    .flatMap((byteBuf -> queryReceiverServer.handleIncommingMessage(byteBuf)))
-                ));
-
-        /*Wait for shutdown if not called from the client (passed an arg)*/
-    if (env.shouldWaitForShutdown(args)) {
-      server.awaitShutdown();
-    }
-
-        /*If not waiting for shutdown, assign the ephemeral port used to a field so that it can be read and used by
-        the caller, if any.*/
-    env.registerServerAddress(server.getServerAddress());
+    queryServer.awaitShutdown();
+    pullServer.awaitShutdown();
   }
 }
