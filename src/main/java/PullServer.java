cbff9e9a410f6f4ac0360b485efcdae9bb1fec2b
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2016-06-09 12:17:11 +0200

    Setup a server for QUERY REQUESTS and FOR PULL REQUESTS for local view

---
+++ src/main/java/PullServer.java
@@ -0,0 +1,43 @@
+import io.netty.buffer.ByteBuf;
+import io.netty.handler.logging.LogLevel;
+import io.reactivex.netty.protocol.tcp.server.TcpServer;
+import java.util.logging.Logger;
+
+/**
+ * Responsible to listen for incoming PULL REQUESTS to answer with the local view
+ *
+ * @author Hannes Dorfmann
+ */
+public class PullServer {
+
+  private static final Logger log = Logger.getLogger(PullServer.class.getName());
+
+  private int port;
+  private TcpServer<ByteBuf, ByteBuf> server;
+
+  public PullServer(int port) {
+    this.port = port;
+  }
+
+  /**
+   * Starts the server to listen for incoming QUERY REQUESTS
+   */
+  public void start() {
+    // TODO implement
+    server = TcpServer.newServer(port)
+        .enableWireLogging(LogLevel.DEBUG)
+        .start(
+            connection ->
+                connection.writeBytesAndFlushOnEach(connection.getInput()
+                    .doOnNext(byteBuf -> log.info("PULL REQUEST local view  received"))
+                    .map(
+                        (byteBuf -> "PULL local view response: Here should be the local view".getBytes()))
+
+                )
+        );
+  }
+
+  public void awaitShutdown(){
+    server.awaitShutdown();
+  }
+}
---
+++ src/main/java/QueryServer.java
@@ -0,0 +1,41 @@
+import io.netty.buffer.ByteBuf;
+import io.netty.handler.logging.LogLevel;
+import io.reactivex.netty.protocol.tcp.server.TcpServer;
+import java.util.logging.Logger;
+
+/**
+ * Responsible to listen for incoming QUERY REQUESTS and to answer them
+ *
+ * @author Hannes Dorfmann
+ */
+public class QueryServer {
+
+  private static final Logger log = Logger.getLogger(QueryServer.class.getName());
+
+  private int port;
+  private TcpServer<ByteBuf, ByteBuf> server;
+
+  public QueryServer(int port) {
+    this.port = port;
+  }
+
+  /**
+   * Starts the server to listen for incoming QUERY REQUESTS
+   */
+  public void start() {
+    server = TcpServer.newServer(port)
+        .enableWireLogging(LogLevel.DEBUG)
+        .start(
+            connection ->
+                connection.writeBytesAndFlushOnEach(connection.getInput()
+                    .doOnNext(byteBuf -> log.info("QUERY REQUEST received"))
+                    .map((byteBuf -> "QUERY Response".getBytes())) // TODO implement
+                )
+        );
+  }
+
+
+  public void awaitShutdown(){
+    server.awaitShutdown();
+  }
+}
