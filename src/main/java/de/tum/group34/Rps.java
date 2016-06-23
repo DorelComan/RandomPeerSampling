c19256d6fb9424b93dc127cf5d8460ef7f689cdf
Dorel Coman <dorel.coman@tum.de>
2016-06-23 13:11:18 +0200

    Brahms modified

--- src/main/java/de/tum/group34/Rps.java
+++ src/main/java/de/tum/group34/Rps.java
@@ -4,6 +4,8 @@ import de.tum.group34.nse.NseClient;
 import io.reactivex.netty.protocol.tcp.client.TcpClient;
 import io.reactivex.netty.protocol.tcp.server.TcpServer;
 
+import java.util.ArrayList;
+
 /**
  * @author Hannes Dorfmann
  */
@@ -13,10 +15,12 @@ public class Rps {
 
     // TODO read config
 
+    NseClient nseClient = new NseClient(TcpClient.newClient("127.0.0.1", 9899));
+    Brahms brahms = new Brahms(new ArrayList<>(), nseClient); // TODO: add peer list from file
+
     QueryServer queryServer = new QueryServer(TcpServer.newServer((11001)));
     PullLocalViewServer pullLocalViewServer = new PullLocalViewServer(TcpServer.newServer(11002));
 
-    NseClient nseClient = new NseClient(TcpClient.newClient("127.0.0.1", 9899));
 
     queryServer.awaitShutdown();
     pullLocalViewServer.awaitShutdown();
