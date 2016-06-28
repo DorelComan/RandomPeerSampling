b43978204d3d506c0eb8efd187b184d5926ca0d1
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2016-06-28 11:53:42 +0200

    Peroidically Gossip Push

---
+++ src/main/java/de/tum/group34/GossipPush.java
@@ -0,0 +1,14 @@
+package de.tum.group34;
+
+import rx.Observable;
+
+/**
+ * @author Hannes Dorfmann
+ */
+public class GossipPush {
+
+  public Observable<Void> sendMessage(long i) {
+    // TODO implement
+    return Observable.empty();
+  }
+}
--- src/main/java/de/tum/group34/Rps.java
+++ src/main/java/de/tum/group34/Rps.java
@@ -3,8 +3,9 @@ package de.tum.group34;
 import de.tum.group34.nse.NseClient;
 import io.reactivex.netty.protocol.tcp.client.TcpClient;
 import io.reactivex.netty.protocol.tcp.server.TcpServer;
-
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+import rx.Observable;
 
 /**
  * @author Hannes Dorfmann
@@ -15,15 +16,24 @@ public class Rps {
 
     // TODO read config
 
+    PullClient pullClient = new PullClient();
+
     NseClient nseClient = new NseClient(TcpClient.newClient("127.0.0.1", 9899));
-    Brahms brahms = new Brahms(new ArrayList<>(), nseClient); // TODO: add peer list from file
+    Brahms brahms =
+        new Brahms(new ArrayList<>(), nseClient, pullClient); // TODO: add peer list from file
 
     QueryServer queryServer = new QueryServer(TcpServer.newServer((11001)));
     PullLocalViewServer pullLocalViewServer = new PullLocalViewServer(TcpServer.newServer(11002));
 
+    GossipPush gossipPush = new GossipPush();
+    Observable.interval(0, 30, TimeUnit.MINUTES)
+        .flatMap(gossipPush::sendMessage)
+        .subscribe((result) -> {
+        }, (error) -> {
+          error.printStackTrace();
+        });
 
     queryServer.awaitShutdown();
     pullLocalViewServer.awaitShutdown();
   }
-
 }
