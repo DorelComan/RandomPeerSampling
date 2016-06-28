51690a19862171b75eb538aa777b8576bb83ef79
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2016-06-28 11:19:36 +0200

    git status

--- src/main/java/de/tum/group34/Brahms.java
+++ src/main/java/de/tum/group34/Brahms.java
@@ -1,149 +1,136 @@
 package de.tum.group34;
 
 import de.tum.group34.nse.NseClient;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import module.Peer;
 import module.Sampler;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Random;
-
 public class Brahms {
 
-    ArrayList<Sampler> samplList; // Sample list
-    ArrayList<Peer> viewList;     // View List
-    Integer samplSize; // l2
-    Integer viewSize; // l1
+  List<Sampler> samplList; // Sample list
+  List<Peer> viewList;     // View List
+  Integer samplSize; // l2
+  Integer viewSize; // l1
 
-    NseClient nseClient;
+  PullClient pullClient;
+  NseClient nseClient;
 
-    Float alfa;
-    Float beta;
-    Float gamma;
-    Integer sizeEst;
+  Float alfa;
+  Float beta;
+  Float gamma;
+  Integer sizeEst;
 
+  /**
+   * Initialization of the algorithm
+   */
+  public Brahms(ArrayList<Peer> list, NseClient nseClient, PullClient pullClient) {
 
-    /**
-     *  Initialization of the algorithm
-     * @param list
-     * @param size
-     */
-    public Brahms(ArrayList<Peer> list, NseClient nseClient){
+    viewList = list;
+    samplList = new ArrayList<>();
+    this.nseClient = nseClient;
+    this.pullClient = pullClient;
 
-        viewList = list;
-        samplList = new ArrayList<>();
-        this.nseClient = nseClient;
+    this.samplSize = nseClient.getNetworkSize().toBlocking().first();
+    this.viewSize = this.samplSize;
 
-        this.samplSize = nseClient.getNetworkSize().toBlocking().first();
-        this.viewSize = this.samplSize;
+    for (int i = 0; i < samplSize; i++)
+      samplList.add(new Sampler());
 
-        for(int i = 0; i < samplSize; i++)
-            samplList.add(new Sampler());
+    updateSample(list);
+  }
 
-        updateSample(list);
+  public void start() {
+
+    List<Peer> pullList;
+    List<Peer> pushList;
+
+    while (true) {            // every iteration to be executed periodically
+      pullList = new ArrayList<>();
+      pushList = new ArrayList<>();
+
+      Integer nmbPushes = Math.round(alfa * viewSize);
+      Integer nmbPulls = Math.round(beta * viewSize);
+      Integer nmbSamples = Math.round(gamma * viewSize);
+
+      for (int i = 0; i < nmbPushes; i++)
+        PushSender.pushRequest(rand(viewList, 1)); // method to be implemented by the Push Sender
+
+      // Send pull requests and save incoming lists in pullList
+
+      pullList = pullClient.makePullRequests(rand(viewList, nmbPulls)).toBlocking().first();
+
+      // Save all push receive in pushList
+
+      pushList = GossipReceiver.getPushList();
+
+      if (pushList.size() <= nmbPushes &&
+          pushList.size() != 0 &&
+          pullList.size() != 0) {
+        viewList = rand(pushList, nmbPushes);
+        viewList.addAll(rand(pullList, nmbPulls));
+        viewList.addAll(randSamples(samplList, nmbSamples));
+      }
     }
+  }
 
-    public void start(){
+  public void validateSamples() {
 
-        ArrayList<Peer> pullList;
-        ArrayList<Peer> pushList;
-
-        while(true){            // every iteration to be executed periodically
-            pullList = new ArrayList<>();
-            pushList = new ArrayList<>();
-
-            Integer nmbPushes =  Math.round(alfa * viewSize);
-            Integer nmbPulls = Math.round(beta * viewSize);
-            Integer nmbSamples = Math.round(gamma * viewSize);
-
-
-            for(int i = 0; i < nmbPushes; i++)
-                 PushSender.pushRequest(rand(viewList,1)); // method to be implemented by the Push Sender
-
-
-            // Send pull requests and save incoming lists in pullList
-
-            // Save all push receive in pushList
-
-            pushList = GossipReceiver.getPushList();
-
-            if(pushList.size() <= nmbPushes &&
-                    pushList.size() != 0 &&
-                        pullList.size() != 0){
-                viewList = rand(pushList, nmbPushes);
-                viewList.addAll(rand(pullList, nmbPulls));
-                viewList.addAll(randSamples(samplList, nmbSamples));
-            }
-        }
+    for (int i = 0; i < samplSize; i++) {
+      if (!samplList.get(i).validate()) {
+        samplList.set(i, new Sampler());
+      }
     }
+  }
 
+  public void updateSample(ArrayList<Peer> list) {
 
-    public void validateSamples(){
+    for (Sampler s : samplList)
+      for (Peer p : list)
+        s.next(p);
+  }
 
-        for(int i = 0; i < samplSize; i++){
-            if(!samplList.get(i).validate())
-                samplList.set(i, new Sampler());
-        }
-    }
+  public static ArrayList<Peer> rand(List<Peer> list, Integer n) {
 
-    public void updateSample(ArrayList<Peer> list){
+    Collections.shuffle(list);
 
-        for(Sampler s: samplList)
-            for(Peer p: list)
-                s.next(p);
+    return (ArrayList<Peer>) list.subList(0, n - 1);
+  }
 
-    }
+  public static ArrayList<Peer> randSamples(List<Sampler> list, Integer n) {
 
-    public static ArrayList<Peer> rand(ArrayList<Peer> list, Integer n){
+    Collections.shuffle(list);
 
-        Collections.shuffle(list);
+    ArrayList<Peer> randList = new ArrayList<>();
 
-        return (ArrayList<Peer>) list.subList(0, n-1);
+    for (int i = 0; i < n; i++)
+      randList.add(list.get(i).sample());
 
-    }
+    return randList;
+  }
 
-    public static ArrayList<Peer> randSamples(ArrayList<Sampler> list, Integer n){
+  public synchronized void setSizeEstimation(Integer size) {
 
-        Collections.shuffle(list);
+    sizeEst = size;
+  }
 
-        ArrayList<Peer> randList = new ArrayList<>();
+  private synchronized Integer getSizeEstim() {
 
-        for(int i=0; i < n; i++)
-            randList.add(list.get(i).sample());
+    return sizeEst;
+  }
 
-        return randList;
-    }
+  public Peer getRandomPeer() {
 
-    public synchronized void setSizeEstimation(Integer size){
+    SecureRandom secureRandom = new SecureRandom();
+    Integer i = secureRandom.nextInt(viewList.size());
 
-        sizeEst = size;
-    }
+    return viewList.get(i);
+  }
 
-    private synchronized Integer getSizeEstim(){
+  public List<Peer> getLocalView() {
 
-        return sizeEst;
-    }
-
-
-    public Peer getRandomPeer(){
-
-
-
-        SecureRandom secureRandom = new SecureRandom();
-        Integer i = secureRandom.nextInt(viewList.size());
-
-        return viewList.get(i);
-    }
-
-    public ArrayList<Peer> getLocalView(){
-
-        return viewList;
-    }
-
-
-
-
+    return viewList;
+  }
 }
