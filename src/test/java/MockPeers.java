a2cf84d74403c45d7dc0a5f9e2a5d93772af6b63
Dorel Coman <dorel.coman@tum.de>
2016-08-11 13:52:34 +0200

    Added FileParser

---
+++ src/test/java/MockPeers.java
@@ -0,0 +1,31 @@
+import de.tum.group34.model.Peer;
+
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+
+public class MockPeers {
+
+    public static Peer getPeer(){
+
+        Peer peer = new Peer();
+        peer.setIpAddress(new InetSocketAddress("127.0.0.33",2555));
+
+        return peer;
+    }
+
+    public static ArrayList<Peer> getPeerList(){
+        ArrayList<Peer> peers = new ArrayList<>();
+        Integer val1, val2;
+        Peer peer;
+
+        for (int i=0; i < 20; i++){
+            val1 = (int)(Math.random()%255);
+            val2 = (int)(Math.random()%255);
+            peer = new Peer();
+            peer.setIpAddress(new InetSocketAddress("127.0." + val1 + "." + val2, val1 + 5000));
+            peers.add(peer);
+        }
+
+        return peers;
+    }
+}
