af14d414d1b0363c86d106a6affde4ee6cc05176
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2016-08-14 05:09:41 +0200

    Encoding fixes

--- src/main/java/de/tum/group34/IpChecker.java
+++ src/main/java/de/tum/group34/IpChecker.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class IpChecker {
 
@@ -12,7 +13,7 @@ public class IpChecker {
     BufferedReader in = null;
     try {
       in = new BufferedReader(new InputStreamReader(
-          whatismyip.openStream()));
+          whatismyip.openStream(), StandardCharsets.UTF_8));
       String ip = in.readLine();
       return ip;
     } finally {
